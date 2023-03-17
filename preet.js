bebc6d8e54197cff27390341932fe2ee8b1c2244
preetraj16185 <119069379+preetraj16185@users.noreply.github.com>
2023-03-17 22:42:19 +0530

    Add files via upload

---
+++ preet.js
@@ -0,0 +1,16 @@
+// alert("welcome to NECKBAND");
+// prompt("enter your name");
+
+// let age=prompt("enter age");
+
+// if(age<=14){
+//     alert("Not allow");
+// } else{
+//     alert(" Allow");
+// }
+
+let n1=prompt("first number");
+let n2=prompt("second number");
+
+z= n1 / n2;
+alert(z);
